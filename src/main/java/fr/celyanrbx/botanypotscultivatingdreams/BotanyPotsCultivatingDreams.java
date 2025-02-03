a8ff30301d93fff9ef4f0343bd9f0669dfcb1d24
CelyanRbx <glmtjulie@gmail.com>
2025-02-03 12:39:08 +0100

    first commit

---
+++ src/main/java/fr/celyanrbx/botanypotscultivatingdreams/BotanyPotsCultivatingDreams.java
@@ -0,0 +1,8 @@
+package fr.celyanrbx.botanypotscultivatingdreams;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BotanyPotsCultivatingDreams.MOD_ID)
+public class BotanyPotsCultivatingDreams {
+    public static final String MOD_ID = "botanypotscultivatingdreams";
+}
