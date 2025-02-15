b536bdcc2eec1d090d13f1c3b67d22d47ba85759
CelyanRbx <glmtjulie@gmail.com>
2025-02-15 03:29:43 +0100

    Initial commit

---
+++ src/main/java/fr/celyanrbx/botanypotscultivatingdreams/BotanyPotsCultivatingDreams.java
@@ -0,0 +1,10 @@
+package fr.celyanrbx.botanypotscultivatingdreams;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BotanyPotsCultivatingDreams implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
